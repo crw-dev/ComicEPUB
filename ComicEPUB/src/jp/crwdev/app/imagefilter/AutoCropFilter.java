9ca7f932c68de573c3f1c03059f0abf1ea4dd370
Naoki Ishikura <teamcrw001dev@gmail.com>
2013-03-27 00:17:15 +0900

    new auto cropping

--- ComicEPUB/src/jp/crwdev/app/imagefilter/AutoCropFilter.java
+++ ComicEPUB/src/jp/crwdev/app/imagefilter/AutoCropFilter.java
@@ -71,7 +71,7 @@ public class AutoCropFilter implements IImageFilter {
 		}
 		//Rectangle subRect = new Rectangle();
 		//Rectangle rect = getAutoCropRect2(image, left, top, right, bottom, subRect);
-		Rectangle rect = getAutoCropRect(image, left, top, right, bottom);
+		Rectangle rect = getAutoCropRect2(image, left, top, right, bottom);
 		
 		if(!rect.isEmpty()){
 			
@@ -208,168 +208,192 @@ public class AutoCropFilter implements IImageFilter {
 	}
 
 	
-	private Rectangle getAutoCropRect2(BufferedImage image, int leftMargin, int topMargin, int rightMargin, int bottomMargin, Rectangle subRect){
+	private Rectangle getAutoCropRect2(BufferedImage image, int leftMargin, int topMargin, int rightMargin, int bottomMargin){
 		Rectangle rect = new Rectangle();
 	
+		int top = deleteCountTop(image, topMargin);
+		int bottom = deleteCountBottom(image, bottomMargin);
+		
+		if(top < 0 && bottom < 0){
+			// not found
+			top = topMargin;
+			bottom = bottomMargin;
+		}
+		else if(top >= 0 && bottom >= 0){
+			// 両方引っかかった
+			top = topMargin - top;
+			bottom = bottomMargin - bottom;
+		}
+		else if(top < 0){
+			// 上は見つからなかったが下は見つかった
+			top = deleteCountTop(image, topMargin + bottom); // 上を再チェック
+			if(top < 0){
+				top = topMargin + bottom; // 見つからなかったので全削り
+			}else{
+				top = topMargin + bottom - top;
+			}
+			bottom = bottomMargin - bottom;
+		}
+		else{
+			// 下は見つからなかったが上は見つかった
+			bottom = deleteCountBottom(image, bottomMargin + top); // 上を再チェック
+			if(bottom < 0){
+				bottom = bottomMargin + top; // 見つからなかったので全削り
+			}else{
+				bottom = bottomMargin + top - bottom;
+			}
+			top = topMargin - top;
+		}
+		
+		
+		int left = deleteCountLeft(image, leftMargin);
+		int right = deleteCountRight(image, rightMargin);
+		
+		if(left < 0 && right < 0){
+			// not found
+			left = leftMargin;
+			right = rightMargin;
+		}
+		else if(left >= 0 && right >= 0){
+			// 両方引っかかった
+			left = leftMargin - left;
+			right = rightMargin - right;
+		}
+		else if(left < 0){
+			// 上は見つからなかったが下は見つかった
+			left = deleteCountLeft(image, leftMargin + right); // 上を再チェック
+			if(left < 0){
+				left = leftMargin + right; // 見つからなかったので全削り
+			}else{
+				left = leftMargin + right - left;
+			}
+			right = rightMargin - right;
+		}
+		else{
+			// 下は見つからなかったが上は見つかった
+			right = deleteCountRight(image, rightMargin + left); // 上を再チェック
+			if(right < 0){
+				right = rightMargin + left; // 見つからなかったので全削り
+			}else{
+				right = rightMargin + left - right;
+			}
+			left = leftMargin - left;
+		}
+
 		int width = image.getWidth();
 		int height = image.getHeight();
 		
-		int leftMargin2 = leftMargin*2;
-		int rightMargin2 = rightMargin*2;
-		int topMargin2 = topMargin*2;
-		int bottomMargin2 = bottomMargin*2;
-		
-		int left = leftMargin == 0 ? width-1 : leftMargin;
-		int right = rightMargin == 0 ? 0 : width - rightMargin - 1;
-		int top = topMargin == 0 ? height-1 : topMargin;
-		int bottom = bottomMargin == 0 ? 0 : height - bottomMargin - 1;
-		
-		int left2 = leftMargin == 0 ? width-1 : leftMargin2;
-		int right2 = rightMargin == 0 ? 0 : width - rightMargin2 - 1;
-		int top2 = topMargin == 0 ? height-1 : topMargin2;
-		int bottom2 = bottomMargin == 0 ? 0 : height - bottomMargin2 - 1;;
-		
-		boolean found = false;
-		for(int y=0; y<height; y+= mCheckOffset){
-			for(int x=0; x<leftMargin && x<left; x+= 1){
-				if(!isWhiteColor(image.getRGB(x, y))){
-					if(x < left){
-						left = x-1;
-						found = true;
-					}
-					break;
-				}
-			}
-		}
-		left = Math.max(0, left);
-
-		if(found){
-			left2 = left;
-		}
-		else{
-			for(int y=0; y<height; y+= mCheckOffset){
-				for(int x=left+1; x<leftMargin2 && x<left2; x+= 1){
-					if(!isWhiteColor(image.getRGB(x, y))){
-						if(x < left2){
-							left2 = x-1;
-							found = true;
-						}
-						break;
-					}
-				}
-			}
-		}
-		
-		
-		found = false;
-		for(int y=0; y<height; y+= mCheckOffset){
-			for(int x=width-1; x>=width-rightMargin-1 && x>=right; x-= 1){
-				if(!isWhiteColor(image.getRGB(x, y))){
-					if(x > right){
-						right = x + 1;
-						found = true;
-					}
-					break;
-				}
-			}
-		}
-		right = Math.min(width-1, right);
-		
-		if(found){
-			right2 = right;
-		}
-		else{
-			for(int y=0; y<height; y+= mCheckOffset){
-				for(int x=right-1; x>=width-rightMargin2-1 && x>=right2; x-= 1){
-					if(!isWhiteColor(image.getRGB(x, y))){
-						if(x > right2){
-							right2 = x + 1;
-							found = true;
-						}
-						break;
-					}
-				}
-			}
-		}
-		
-		found = false;
-		for(int x=0; x<width; x+= mCheckOffset){
-			for(int y=0; y<topMargin && y<top; y+= 1){
-				if(!isWhiteColor(image.getRGB(x, y))){
-					if(y < top){
-						top = y - 1;
-						found = true;
-					}
-					break;
-				}
-			}
-		}
-		top = Math.max(0, top);
-		
-		if(found){
-			top2 = top;
-		}
-		else{
-			for(int x=0; x<width; x+= mCheckOffset){
-				for(int y=top+1; y<topMargin2 && y<top2; y+= 1){
-					if(!isWhiteColor(image.getRGB(x, y))){
-						if(y < top2){
-							top2 = y - 1;
-							found = true;
-						}
-						break;
-					}
-				}
-			}
-		}
-		
-		
-		found = false;
-		for(int x=0; x<width; x+= mCheckOffset){
-			for(int y=height-1; y>=height-bottomMargin-1  && y>=bottom; y-= 1){
-				if(!isWhiteColor(image.getRGB(x, y))){
-					if(y > bottom){
-						bottom = y + 1;
-						found = true;
-					}
-					break;
-				}
-			}
-		}
-		bottom = Math.min(height-1, bottom);
-		
-		if(found){
-			bottom2 = bottom;
-		}
-		else{
-			for(int x=0; x<width; x+= mCheckOffset){
-				for(int y=bottom-1; y>=height-bottomMargin2-1  && y>=bottom2; y-= 1){
-					if(!isWhiteColor(image.getRGB(x, y))){
-						if(y > bottom2){
-							bottom2 = y + 1;
-							found = true;
-						}
-						break;
-					}
-				}
-			}
-		}
-
-		if(left >= right){
-			left = 0;
-			right = width-1;
-		}
-		if(top >= bottom){
-			top = 0;
-			bottom = height-1;
-		}
-		
-		rect.setBounds(left, top, right-left, bottom-top);
-		subRect.setBounds(left2, top2, right2-left2, bottom2-top2);
+		rect.setBounds(left, top, width - left - right - 1, height - top - bottom - 1);
 		
 		return rect;
 	}
 
+	
+	private int deleteCountLeft(BufferedImage image, int max){
+		int width = image.getWidth();
+		int height = image.getHeight();
+		
+		int left = max;
+		boolean found = false;
+		for(int y=0; y<height; y++){
+			for(int x=0; x<max && x<left; x++){
+				if(!isWhiteColor(image.getRGB(x, y))){
+					if(x < left){
+						left = x;
+						found = true;
+						break;
+					}
+				}
+			}
+		}
+		
+		if(found){
+			return (max - left);
+		}
+		else{
+			return -1;
+		}
+	}
+
+	private int deleteCountTop(BufferedImage image, int max){
+		int width = image.getWidth();
+		int height = image.getHeight();
+		
+		int top = max;
+		boolean found = false;
+		for(int x=0; x<width; x++){
+			for(int y=0; y<max && y<top; y++){
+				if(!isWhiteColor(image.getRGB(x, y))){
+					if(y < top){
+						top = y;
+						found = true;
+						break;
+					}
+				}
+			}
+		}
+		
+		if(found){
+			return (max - top);
+		}
+		else{
+			return -1;
+		}
+	}
+
+	private int deleteCountRight(BufferedImage image, int max){
+		int width = image.getWidth();
+		int height = image.getHeight();
+		
+		int right = max;
+		boolean found = false;
+		for(int y=0; y<height; y++){
+			for(int x=0; x<max && x<right; x++){
+				if(!isWhiteColor(image.getRGB(width - x - 1, y))){
+					if(x < right){
+						right = x;
+						found = true;
+						break;
+					}
+				}
+			}
+		}
+		
+		if(found){
+			return (max - right);
+		}
+		else{
+			return -1;
+		}
+	}
+
+	private int deleteCountBottom(BufferedImage image, int max){
+		int width = image.getWidth();
+		int height = image.getHeight();
+		
+		int bottom = max;
+		boolean found = false;
+		for(int x=0; x<width; x++){
+			for(int y=0; y<max && y<bottom; y++){
+				if(!isWhiteColor(image.getRGB(x, height - y - 1))){
+					if(y < bottom){
+						bottom = y;
+						found = true;
+						break;
+					}
+				}
+			}
+		}
+		
+		if(found){
+			return (max - bottom);
+		}
+		else{
+			return -1;
+		}
+	}
+
+	
 	public boolean isWhiteColor(int color){
 		int r = (color >> 16) & 0xff;
 		int g = (color >> 8) & 0xff;
